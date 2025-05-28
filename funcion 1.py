c506b37cdba0c619f5c82af20c6cd5a7f9c5dc4c
Ulises Baudino <213527800+ulises-404@users.noreply.github.com>
2025-05-28 09:23:31 -0300

    Update funcion 1.py
    monto final

--- funcion 1.py
+++ funcion 1.py
@@ -1,3 +1,74 @@
+def calcular_monto_final(codigo_iso, monto_nominal):
+    # Inicializar variables
+    mensaje = ""
+    monto_base = 0
+    monto_final = 0
+    
+    if codigo_iso == codigo_identificacion[0]:  # ARS
+        mensaje = "Moneda valida"
+        comision = round((monto_nominal * 5) / 100, 2)
+        monto_base = monto_nominal - comision
+
+    elif codigo_iso == codigo_identificacion[1]:  # USD
+        mensaje = "Moneda valida"
+        comision = round((monto_nominal * 7) / 100, 2)
+        monto_base = monto_nominal - comision
+
+    elif codigo_iso == codigo_identificacion[2]:  # EUR
+        mensaje = "Moneda valida"
+        comision = round((monto_nominal * 7) / 100, 2)
+        monto_base = monto_nominal - comision
+
+    elif codigo_iso == codigo_identificacion[3]:  # GBP
+        mensaje = "Moneda valida"
+        comision = round((monto_nominal * 9) / 100, 2)
+        monto_base = monto_nominal - comision
+
+    elif codigo_iso == codigo_identificacion[4]:  # JPY
+        if monto_nominal >= 15000:
+            if monto_nominal <= 500000:
+                mensaje = "Moneda valida"
+                comision = round((monto_nominal * 9) / 100, 2)
+                if comision > 950000:
+                    comision = 950000
+                monto_base = monto_nominal - comision
+            elif monto_nominal <= 1500000:
+                mensaje = "Moneda valida"
+                comision = round((monto_nominal * 7.8) / 100, 2)
+                if comision > 950000:
+                    comision = 950000
+                monto_base = monto_nominal - comision
+            elif monto_nominal <= 10000000:
+                mensaje = "Moneda valida"
+                comision = round((monto_nominal * 5.5) / 100, 2)
+                if comision > 950000:
+                    comision = 950000
+                monto_base = monto_nominal - comision
+            else:
+                mensaje = "Moneda valida"
+                comision = round((monto_nominal * 5) / 100, 2)
+                if comision > 950000:
+                    comision = 950000
+                monto_base = monto_nominal - comision
+        else:
+            mensaje = "Monto minimo para JPY no alcanzado"
+            monto_base = 0
+    else:
+        mensaje = "Moneda no autorizada"
+        monto_base = 0
+
+    if monto_base > 500000:
+        impuesto = (monto_base * 21) / 100
+        monto_final = monto_base - impuesto
+    else:
+        monto_final = monto_base
+    
+    # Retornar monto_final solo si moneda es válida y monto_base > 0
+    if mensaje == "Moneda valida" and monto_base > 0:
+        return monto_final
+    else:
+        return 0
+
 def operacioes_validas(monto_final):
     c1 = suma_monto_final = 0
     c1 += 1
@@ -47,7 +118,8 @@ def principal():
         nombre, codigo_id, codigo_iso, monto_nominal, alg_comision, alg_impuesto = extraer_datos_linea(linea)
 
     if es_moneda(codigo_iso):
-        operaciones_validas()
+        monto_final = calcular_monto_final()
+        operaciones_validas(monto_final)
 
     archivo.close()
 
